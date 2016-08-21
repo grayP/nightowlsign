588e0c9f0d92d9e80c641612b059dfc31b0219d5
Gray Pritchett <gray.pritchett@optusnet.com.au>
2016-08-21 21:26:07 +1000

    datepicker

---
+++ nightowlsign.data/PartialClasses/PartialClasses.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+
+namespace nightowlsign.data
+{
+    [MetadataType(typeof(ScheduleMetaData))]
+    public partial class Schedule
+    {
+
+    }
+}
