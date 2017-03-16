ae082baba5c739cf3cc6fcfbb9e17178a3c562d1
Gray Pritchett <gray.pritchett@smsmt.com>
2017-03-16 19:11:35 +1000

    master commit

---
+++ nightowlsign.data/Models/StoreScheduleLog/StoreScheduleLogManager.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+
+
+namespace nightowlsign.data.Models.StoreScheduleLog
+{
+    public class StoreScheduleLogManager
+    {
+        public string ErrorMessage { get; set; }
+        public data.StoreScheduleLog Entity { get; set; }
+
+
+
+        public List<data.StoreScheduleLog> Get(data.StoreScheduleLog storeScheduleLog)
+        {
+            using (nightowlsign_Entities db = new nightowlsign_Entities())
+            {
+                var ret = db.StoreScheduleLogs.OrderBy(x => x.ScheduleName).ToList<data.StoreScheduleLog>();
+                ret = ret.FindAll(p => p.ScheduleName.ToLower().StartsWith(storeScheduleLog.ScheduleName));
+                return ret;
+            }
+        }
+
+
+        private data.StoreScheduleLog GetStoreScheduleLog(int scheduleStoreLogId)
+        {
+            using (nightowlsign_Entities db = new nightowlsign_Entities())
+            {
+                return db.StoreScheduleLogs.Find(scheduleStoreLogId);
+            }
+        }
+
+        public bool Insert()
+        {
+            try
+            {
+                using (nightowlsign_Entities db = new nightowlsign_Entities())
+                {
+                    data.StoreScheduleLog newStoreScheduleLog = new data.StoreScheduleLog()
+                    {
+                        ScheduleName = Entity.ScheduleName.Trim(),
+                        DateInstalled = DateTime.Now,
+                        StoreId = Entity.StoreId,
+                        InstalledOk = true,
+                        ScheduleId = Entity.ScheduleId
+                    };
+                    db.StoreScheduleLogs.Add(newStoreScheduleLog);
+                    db.SaveChanges();
+                }
+                return true;
+            }
+            catch (Exception ex)
+            {
+                ErrorMessage = ex.Message;
+                return false;
+            }
+        }
+
+
+        public bool Delete(data.StoreScheduleLog entity)
+        {
+            bool ret = false;
+            using (nightowlsign_Entities db = new nightowlsign_Entities())
+            {
+                db.StoreScheduleLogs.Attach(entity);
+                db.StoreScheduleLogs.Remove(entity);
+                db.SaveChanges();
+                ret = true;
+            }
+            return ret;
+        }
+
+        internal void DeleteLog(int scheduleId)
+        {
+            try
+            {
+                using (nightowlsign_Entities db = new nightowlsign_Entities())
+                {
+                    db.StoreScheduleLogs.RemoveRange(db.StoreScheduleLogs.Where(x => x.ScheduleId == scheduleId));
+                    db.SaveChanges();
+                 }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
+        }
+    }
+}
+
--- nightowlsigns/Properties/AssemblyInfo.cs
+++ nightowlsigns/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("nightowlsign")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Night Owl Sign management System")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Gray Road Consulting")]
 [assembly: AssemblyProduct("nightowlsign")]
-[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Revision and Build Numbers
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("2.0.0.1")]
+[assembly: AssemblyFileVersion("2.0.0.1")]
