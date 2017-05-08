e88d2b3e50b123a344389c7f8b44870788b114c3
Gray Pritchett <gray.pritchett@smsmt.com>
2017-05-08 13:07:08 +1000

    Identity models

--- nightowlsigns/Models/IdentityModels.cs
+++ nightowlsigns/Models/IdentityModels.cs
@@ -21,7 +21,7 @@ namespace nightowlsign.Models
     public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
         public ApplicationDbContext()
-            : base("DefaultConnection", throwIfV1Schema: false)
+            : base("LocalSqlServer", throwIfV1Schema: false)
         {
         }
 
